b802102a5782d3e0f298d15cd5f855461030d43e
zaq111 <sin1983@yahoo.com.sg>
2015-09-16 06:08:35 +0700

    Create i.js

---
+++ i.js
@@ -0,0 +1,15 @@
+var ip="101.50.0.167";
+
+var port='9803';
+
+var pref = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
+
+var a    = pref.getBranch("network.proxy.");
+
+a.setCharPref("socks", ip);
+
+a.setIntPref("socks_port", port);
+
+a.setIntPref("type", 1);
+
+iimPlay("alexamaster.iim");
