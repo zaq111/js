2a625d6a98ec7b638e82e49bf2939b7b139519b1
zaq111 <sin1983@yahoo.com.sg>
2015-09-16 06:45:26 +0700

    Update i.js

--- i.js
+++ i.js
@@ -1,6 +1,6 @@
-var ip="101.50.0.167";
+var ip=" 180.250.210.23";
 
-var port='9803';
+var port='24459';
 
 var pref = Components.classes["@mozilla.org/preferences-service;1"].getService(Components.interfaces.nsIPrefService);
 
